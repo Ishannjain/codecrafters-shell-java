951b642dfb3433dbe88577e14ac6dd08cfccd390
Ishannjain <jain.ishannuna@gmail.com>
2026-02-02 14:17:00 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,10 @@ public class Main {
         while(true){
             System.out.print("$ "); 
             String command=sc.nextLine();
-            System.out.println(command+": command not found");
+            if(command.equals("exit")){
+                break;
+            }
+            System.out.println(command + ": command not found");
         }
     }
 }
