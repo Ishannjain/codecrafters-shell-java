aa55087eb24c72d60936524c20d930a0e2c9a3d4
Ishannjain <jain.ishannuna@gmail.com>
2026-02-02 14:38:24 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,17 @@ public class Main {
                 System.out.println(substr);
                 continue;
             }
+            if(command.startsWith("type ")){
+                String substr=command.substring(5);
+                if(substr.equals("echo") || substr.equals("exit") || substr.equals("type")){
+                    System.out.println(substr+" is a shell builtin");
+                    continue;
+                }
+                else{
+                    System.out.println(substr+": not found");
+                    continue;
+                }
+            }
             System.out.println(command + ": command not found");
         }
     }
